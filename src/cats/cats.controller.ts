d3dfa2824ca90c1f91132cfc82538c6b38611a3b
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-06-29 12:09:22 +0300

    feat: testing uuid pipe

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -5,6 +5,7 @@ import {
   Body,
   Param,
   ParseIntPipe,
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
@@ -16,7 +17,19 @@ export class CatsController {
 
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
+    console.log('id:', id);
+    console.log('id Type:', typeof id);
+
     const cat = this.catsService.findOne(id);
+    return cat;
+  }
+
+  @Get('byUuid/:uuid')
+  findOneByUUID(@Param('uuid', ParseUUIDPipe) uuid: string) {
+    console.log('UUID:', uuid);
+    console.log('UUID Type:', typeof uuid);
+
+    const cat = this.catsService.findOne(1);
     return cat;
   }
 
