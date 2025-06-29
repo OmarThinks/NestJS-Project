3002b6adee6b362f10e4254af136ffc51ed6caba
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-06-29 09:32:04 +0300

    feat: used DTO

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateCatDto } from './DTOs/create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   @Post()
-  create(): string {
+  async create(@Body() createCatDto: CreateCatDto) {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    console.log(createCatDto);
+
     return 'This action adds a new cat';
   }
-
   @Get()
   findAll(): string {
     return 'This action returns all cats';
